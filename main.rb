b1cabdfa5194d86c7fcc1429849881a44141997b
Jack Christensen <jack@jackchristensen.com>
2018-05-31 16:38:30 -0500

    Testing fork

--- main.rb
+++ main.rb
@@ -6,7 +6,10 @@ get '/' do
   x1, x2, x3 = m.int_var_array(3, 0..Cbc::INF)
   m.maximize(10*x1 + 6*x2 + 4*x3)
   m.enforce(x1 + x2 + x3 <= 100)
-  m.to_problem.solve
+  # Thread.new do
+    Process.fork { m.to_problem.solve; exit! }
+    Process.wait
+  # end
 
-  "doesn't get here on Mac"
+  "doesn't get here on Mac 4"
 end
